37c6573ea4a9034b4024c17ccb8b31a7b4d4761d
Sean Scott <sean@icansee.co.uk>
2020-03-21 17:50:15 +0000

    flexible section

--- pinnaclewp/wp-content/themes/pinnacle2020/flexible/seo/seo-text_left_image_right.php
+++ pinnaclewp/wp-content/themes/pinnacle2020/flexible/seo/seo-text_left_image_right.php
@@ -1,10 +1,10 @@
 <div class="container">
   <div class="grid">
-    <div class="image-left">
-      <?php $image = get_sub_field('image_left'); ?>
+    <div class="image-right">
+      <?php $image = get_sub_field('image_right'); ?>
         <img src="<?php $image['url'];?>" alt="<?php $image['alt'];?>" />
     </div>
-    <div class="content-right">
+    <div class="content-left">
       <h2><?php the_sub_field('section_title'); ?></h2>
       <?php the_sub_field('section_content'); ?>
       <?php get_template_part('flexible/seo/buttons'); ?>
