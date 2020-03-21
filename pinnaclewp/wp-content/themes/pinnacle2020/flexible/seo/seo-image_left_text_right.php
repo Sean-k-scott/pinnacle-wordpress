5032ad7484396ad013d48a133c09a9ef3da0352e
Sean Scott <sean@icansee.co.uk>
2020-03-21 17:41:27 +0000

    flexible templates

---
+++ pinnaclewp/wp-content/themes/pinnacle2020/flexible/seo/seo-image_left_text_right.php
@@ -0,0 +1,13 @@
+<div class="container">
+  <div class="grid">
+    <div class="image-left">
+      <?php $image = get_sub_field('image_left'); ?>
+        <img src="<?php $image['url'];?>" alt="<?php $image['alt'];?>" />
+    </div>
+    <div class="content-right">
+      <h2><?php the_sub_field('section_title'); ?></h2>
+      <?php the_sub_field('section_content'); ?>
+      <?php get_template_part('flexible/seo/buttons'); ?>
+    </div>
+  </div>
+</div>
